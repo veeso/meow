f8725e9c96ee28c84d25e94e2bd64b420d2e1b4b
veeso <christian.visintin1997@gmail.com>
2022-11-10 18:34:46 +0100

    contract impl

---
+++ app/src/App.tsx
@@ -0,0 +1,28 @@
+
+import * as React from 'react';
+import { hot } from "react-hot-loader/root";
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+interface Props {
+   name:
+    string
+}
+
+class App extends React.Component<Props> {
+  render() {
+    const { name } = this.props;
+    return (
+      <>
+        <h1>
+          Hello {name}
+        </h1>
+        <button type="button" class="btn btn-primary">
+          This is a bootstrap button
+        </button>
+      </>
+    );
+  }
+}
+
+export default hot(App);
---
+++ app/webpack.config.js
@@ -0,0 +1,66 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.tsx'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.ts(x)?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader'
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    'static': {
+      directory: './dist'
+    }
+  },
+  resolve: {
+    extensions: [
+      '.tsx',
+      '.ts',
+      '.js'
+    ],
+    alias: {
+      'react-dom': '@hot-loader/react-dom'
+    }
+  }
+};
+
+module.exports = config;
