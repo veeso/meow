12bca5dbaa4a450d846b0d256c245a3eefa70fbf
veeso <christian.visintin1997@gmail.com>
2022-11-14 10:16:31 +0100

    debouncer

--- app/src/js/components/Search.tsx
+++ app/src/js/components/Search.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { hot } from "react-hot-loader/root";
+import debounce from "lodash.debounce";
 import styled from "styled-components";
 import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
@@ -13,8 +14,6 @@ import Results from "./Search/Results";
 const Container = styled.div`
   border: 1px solid dodgerblue;
   border-radius: 1em;
-  display: flex;
-  flex-direction: row;
 
   input {
     border: 0;
@@ -41,61 +40,66 @@ interface Props {
 const Search = (props: Props) => {
   const [query, setQuery] = React.useState<string>("");
   const [results, setResults] = React.useState<Array<SearchResult>>();
-  const [searching, setSearching] = React.useState<boolean>(false);
   const [error, setError] = React.useState<boolean>(false);
 
-  const onType = (event: React.FormEvent<EventTarget>) => {
-    const text = (event.target as HTMLInputElement).value;
-    setQuery(text);
+  const changeHandler = (event: React.FormEvent<EventTarget>) => {
+    setQuery((event.target as HTMLInputElement).value);
   };
+
+  const debouncedEventHandler = React.useMemo(
+    () => debounce(changeHandler, 300),
+    []
+  );
 
   React.useEffect(() => {
     if (query.length > 0) {
-      if (!searching) {
-        setSearching(true);
-        props
-          .onSearch(query)
-          .then((queryResults) => {
-            setResults(queryResults);
-          })
-          .catch(() => {
-            setError(true);
-          });
-      }
+      props
+        .onSearch(query)
+        .then((queryResults) => {
+          setResults(queryResults);
+        })
+        .catch(() => {
+          setError(true);
+        });
     }
   }, [query]);
 
   React.useEffect(() => {
     if (results) {
-      setSearching(false);
       setError(false);
     }
   }, [results]);
+
+  React.useEffect(() => {
+    return () => {
+      debouncedEventHandler.cancel();
+    };
+  }, []);
 
   return (
     <>
       <Container>
         <Form>
           <Form.Group controlId="search">
-            <Row>
+            <Row lg={12}>
               <Col lg={1}>
                 <SearchIcon>
                   <MagnifyingGlassIcon width={24} />
                 </SearchIcon>
               </Col>
-              <Col lg={11}>
+              <Col lg={10}>
                 <Form.Control
                   placeholder="Search..."
-                  onChange={onType}
+                  onChange={debouncedEventHandler}
                   size="lg"
-                  value={query}
+                  style={{ width: "100%" }}
                 />
               </Col>
             </Row>
           </Form.Group>
         </Form>
       </Container>
-      <Popover show={results !== undefined || error} placement="bottom">
+      <Popover hidden={results === undefined && !error} placement="bottom">
         <Popover.Header hidden={!error}>
           <Alert variant="danger">Unable to load search results</Alert>
         </Popover.Header>
